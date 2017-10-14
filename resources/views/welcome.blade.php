a6c22c8a29e3814a55c18ac214434335b136c5b5
Adrian Preston <prestaj0@sewanee.edu>
2017-10-14 17:14:51 -0400

    take_snapshot() added to welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -14,13 +14,12 @@
 <body>
 <div id="results">Your captured image will appear here...</div>
 
-<h1>WebcamJS Test Page</h1>
-<h3>Demonstrates simple 320x240 capture &amp; display</h3>
+<h1>Image Capture Test Page</h1>
 
 <div id="my_camera"></div>
 
 <!-- First, include the Webcam.js JavaScript Library -->
-<script type="text/javascript" src="{{public_path('js/webcam.js')}}"></script>
+<script type="text/javascript" src="{{asset('js/webcam.js')}}"></script>
 
 <!-- Configure a few settings and attach camera -->
 <script language="JavaScript">
@@ -28,26 +27,50 @@
         width: 320,
         height: 240,
         image_format: 'jpeg',
-        jpeg_quality: 90
+        jpeg_quality: 100
     });
     Webcam.attach( '#my_camera' );
 </script>
 
-<!-- A button for taking snaps -->
-<form>
-    <input type=button value="Take Snapshot" onClick="take_snapshot()">
-</form>
 
 <!-- Code to handle taking the snapshot and displaying it locally -->
 <script language="JavaScript">
+
+    var subscriptionKey = "33ae7357b50b4045a712bc8c56d1f600";
+    var uriBase = "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/";
+
     function take_snapshot() {
         // take snapshot and get image data
         Webcam.snap( function(data_uri) {
-            // display results in page
-            document.getElementById('results').innerHTML =
-                '<h2>Here is your image:</h2>' +
-                '<img src="'+data_uri+'"/>';
+            // call detect from api on image
+            getFaceId('detect',+data_uri+);
         } );
+    }
+
+    function getFaceId(api, faceUrl) {
+        var baseUri = uriBase + api;
+        var params = {
+            returnFaceId: "true"
+        };
+        var faceid = -1;
+        $.ajax({
+            async: false,
+            url: baseUri + "?" + $.param(params),
+            beforeSend: function(xhrObj){
+                xhrObj.setRequestHeader("Content-Type","application/json");
+                xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", subscriptionKey);
+            },
+            type: "POST",
+            data: '{"url": ' + '"' + faceUrl + '"}'
+        })
+                .done(function(data) {
+                    console.log(data);
+                    faceid = data[0].faceId;
+                })
+                .fail(function(jqXHR, textStatus, errorThrown) {
+                    console.log("Failed", textStatus);
+                });
+        return faceid;
     }
 </script>
 
