ed0be6b682f5c4243c113d8b3a2256995f2826ac
Junghoo Kim <kjh9491@hotmail.com>
2017-10-15 03:27:52 -0500

    Reduced the size of speech api javascript

--- SpeechToText-WebSockets-Javascript-master/samples/browser/Sample.js
+++ SpeechToText-WebSockets-Javascript-master/samples/browser/Sample.js
@@ -5,30 +5,28 @@ function prompt(message){
     bingClientTTS.synthesize(message);
 }
 
-
 var startBtn, stopBtn, hypothesisDiv, phraseDiv, statusDiv;
-var key, languageOptions, formatOptions, recognitionMode, inputSource, filePicker;
+var key, formatOptions, recognitionMode, inputSource, filePicker;
 var SDK;
 var recognizer;
 var previousSubscriptionKey;
-
-
 
 function Initialize(onComplete) {
     require(["Speech.Browser.Sdk"], function(SDK) {
         onComplete(SDK);
     });
 }
+
 function RecognizerSetup(SDK, recognitionMode, language, format, subscriptionKey) {
-    
-    recognitionMode = SDK.RecognitionMode.Dictation; 
+
+    recognitionMode = SDK.RecognitionMode.Dictation;
 
     var recognizerConfig = new SDK.RecognizerConfig(
         new SDK.SpeechConfig(
             new SDK.Context(
                 new SDK.OS(navigator.userAgent, "Browser", null),
                 new SDK.Device("SpeechSample", "SpeechSample", "1.0.00000"))),
-        recognitionMode, language, format); 
+        recognitionMode, language, format);
 
     var authentication = new SDK.CognitiveSubscriptionKeyAuthentication(subscriptionKey);
 
@@ -39,132 +37,89 @@ function RecognizerSetup(SDK, recognitionMode, language, format, subscriptionKey
         return SDK.CreateRecognizerWithFileAudioSource(recognizerConfig, authentication, files[0]);
     }
 }
+startBtn.addEventListener("click", function () {
+    if (!recognizer || previousSubscriptionKey != '1f09e238de7842b685c4d4a303ee58f9') {
+        previousSubscriptionKey = '1f09e238de7842b685c4d4a303ee58f9';
+        Setup();
+    }
 
-document.addEventListener("DOMContentLoaded", function () {
-    createBtn = document.getElementById("createBtn");
-    startBtn = document.getElementById("startBtn");
-    stopBtn = document.getElementById("stopBtn");
-    phraseDiv = document.getElementById("phraseDiv");
-    hypothesisDiv = document.getElementById("hypothesisDiv");
-    statusDiv = document.getElementById("statusDiv");
-    key = document.getElementById("key");
-    languageOptions = document.getElementById("languageOptions");
-    formatOptions = document.getElementById("formatOptions");
-    inputSource = document.getElementById("inputSource");
-    recognitionMode = document.getElementById("recognitionMode");
-    filePicker = document.getElementById('filePicker');
+    hypothesisDiv.innerHTML = "";
+    phraseDiv.innerHTML = "";
+    RecognizerStart(SDK, recognizer);
+    startBtn.disabled = true;
+    stopBtn.disabled = false;
+});
 
-    languageOptions.addEventListener("change", Setup);
-    formatOptions.addEventListener("change", Setup);
-    recognitionMode.addEventListener("change", Setup);
+stopBtn.addEventListener("click", function () {
+    RecognizerStop(SDK, recognizer);
+    stopBtn.disabled = true;
+});
 
-    startBtn.addEventListener("click", function () {
-        if (key.value == "" || key.value == "YOUR_BING_SPEECH_API_KEY") {
-            alert("Please enter your Bing Speech subscription key!");
-            return;
-        }
-        if (!recognizer || previousSubscriptionKey != key.value) {
-                previousSubscriptionKey = key.value;
-                Setup();
-            }
-
-        hypothesisDiv.innerHTML = "";
-        phraseDiv.innerHTML = "";
-        RecognizerStart(SDK, recognizer);
-        startBtn.disabled = true;
-        stopBtn.disabled = false;
-                   
-    });
-
-    key.addEventListener("focus", function () {
-       if (key.value == "YOUR_BING_SPEECH_API_KEY") {
-           key.value = "";
-       }
-    });
-
-    key.addEventListener("focusout", function () {
-       if (key.value == "") {
-           key.value = "YOUR_BING_SPEECH_API_KEY";
-       }
-    });
-
-    filePicker.addEventListener("change", function () {
-    });
-
-    stopBtn.addEventListener("click", function () {
-        RecognizerStop(SDK, recognizer);
-        stopBtn.disabled = true;
-    });
-
-    Initialize(function (speechSdk) {
-        SDK = speechSdk;
-        startBtn.disabled = false;
-    });
+Initialize(function (speechSdk) {
+    SDK = speechSdk;
+    startBtn.disabled = false;
 });
 
 // Start the recognition
 function RecognizerStart(SDK, recognizer) {
     recognizer.Recognize((event) => {
         switch (event.Name) {
-            case "RecognitionTriggeredEvent" :
-                UpdateStatus("Initializing");
-                break;
-            case "ListeningStartedEvent" :
-                UpdateStatus("Listening");
-                break;
-            case "RecognitionStartedEvent" :
-                UpdateStatus("Listening_Recognizing");
-                break;
-            case "SpeechStartDetectedEvent" :
-                UpdateStatus("Listening_DetectedSpeech_Recognizing");
-                console.log(JSON.stringify(event.Result));
-                break;
-            case "SpeechHypothesisEvent" :
-                UpdateRecognizedHypothesis(event.Result.Text, false);
-                console.log(JSON.stringify(event.Result));
-                break;
-            case "SpeechFragmentEvent" :
-                UpdateRecognizedHypothesis(event.Result.Text, true);
-                console.log(JSON.stringify(event.Result));
-                break;
-            case "SpeechEndDetectedEvent" :
-                OnSpeechEndDetected();
-                UpdateStatus("Processing_Adding_Final_Touches");
+    case "RecognitionTriggeredEvent" :
+        UpdateStatus("Initializing");
+        break;
+    case "ListeningStartedEvent" :
+        UpdateStatus("Listening");
+        break;
+    case "RecognitionStartedEvent" :
+        UpdateStatus("Listening_Recognizing");
+        break;
+    case "SpeechStartDetectedEvent" :
+        UpdateStatus("Listening_DetectedSpeech_Recognizing");
+        console.log(JSON.stringify(event.Result));
+        break;
+    case "SpeechHypothesisEvent" :
+        UpdateRecognizedHypothesis(event.Result.Text, false);
+        console.log(JSON.stringify(event.Result));
+        break;
+    case "SpeechFragmentEvent" :
+        UpdateRecognizedHypothesis(event.Result.Text, true);
+        console.log(JSON.stringify(event.Result));
+        break;
+    case "SpeechEndDetectedEvent" :
+        OnSpeechEndDetected();
+        UpdateStatus("Processing_Adding_Final_Touches");
 
-                console.log(JSON.stringify(event.Result));
-                break;
-            case "SpeechSimplePhraseEvent" :
-                UpdateRecognizedPhrase(JSON.stringify(event.Result, null, 3));
-                break;
-            case "SpeechDetailedPhraseEvent" :
-                UpdateRecognizedPhrase(JSON.stringify(event.Result, null, 3));
-                break;
-            case "RecognitionEndedEvent" :
-                OnComplete();
-                UpdateStatus("Idle");
-                console.log(JSON.stringify(event));
-                break;
-            default:
-                console.log(JSON.stringify(event));
-        }
-    })
-    .On(() => {
+        console.log(JSON.stringify(event.Result));
+        break;
+    case "SpeechSimplePhraseEvent" :
+        UpdateRecognizedPhrase(JSON.stringify(event.Result, null, 3));
+        break;
+    case "SpeechDetailedPhraseEvent" :
+        UpdateRecognizedPhrase(JSON.stringify(event.Result, null, 3));
+        break;
+    case "RecognitionEndedEvent" :
+        OnComplete();
+        UpdateStatus("Idle");
+        console.log(JSON.stringify(event));
+        break;
+    default:
+        console.log(JSON.stringify(event));
+    }
+})
+.On(() => {
         // The request succeeded. Nothing to do here.
     },
-    (error) => {
+        (error) => {
         console.error(error);
     });
 }
 
-// Stop the Recognition.
 function RecognizerStop(SDK, recognizer) {
     recognizer.AudioSource.TurnOff();
 }
 
-
-
 function Setup() {
-    recognizer = RecognizerSetup(SDK, recognitionMode.value, languageOptions.value, SDK.SpeechResultFormat[formatOptions.value], key.value);
+    recognizer = RecognizerSetup(SDK, recognitionMode.value, "en-US", SDK.SpeechResultFormat[formatOptions.value], key.value);
 }
 
 function UpdateStatus(status) {
@@ -172,9 +127,9 @@ function UpdateStatus(status) {
 }
 
 function UpdateRecognizedHypothesis(text, append) {
-    if (append) 
+    if (append)
         hypothesisDiv.innerHTML += text + " ";
-    else 
+    else
         hypothesisDiv.innerHTML = text;
 
     var length = hypothesisDiv.innerHTML.length;
